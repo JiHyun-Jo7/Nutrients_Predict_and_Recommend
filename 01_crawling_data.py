d3f5850d6bd7dcbedfd2ad84dfa960ba935e6053
JiHyun-Jo7 <jihn2824@gmail.com>
2023-11-08 09:34:42 +0900

    crawling_nutrient_effect,category,name_datas

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -33,10 +33,10 @@ df_nutrients = pd.DataFrame()
 for i in range(len(category)):                          # 카테고리 변경
     # 데이터 프레임 및 리스트 초기화
     df_nutrient = pd.DataFrame()
-    titles = []
+    effect = []
     product_names = []
 
-    for j in range(0, 7):                   # 이후 예측 데이터를 위해 최대 페이지 수를 5 페이지로 제한
+    for j in range(0, 7):                   # 이후 예측 데이터를 위해 최대 페이지 수 제한
         section_url = url + 'c/{}?p={}'.format(category[i], j)
         driver.get(section_url)             # 페이지 변경
         time.sleep(3)
@@ -58,7 +58,7 @@ for i in range(len(category)):                          # 카테고리 변경
             for y in z.find_all({'a'}):
                 if y.get('href') is not None:
                     product_url.append(y.get('href'))
-        del product_url[0]                  # 관계 없는 url 삭제
+        del product_url[0]              # 관계 없는 url 삭제
         for k in range(0, 48):          # 마지막 페이지 제품 수가 48개 미만일 경우 range=(0, product)
             try:
                 driver.get(product_url[k])
@@ -74,7 +74,7 @@ for i in range(len(category)):                          # 카테고리 변경
                 #                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div').text
                 # 제품 설명 전처리 (한글, 영어(대,소문자), 숫자만 수집. 그 외는 공백 처리)
                 # nutrient_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data)
-                # titles.append(nutrient_data)
+                # effect.append(nutrient_data)
 
                 # 2. 상품 설명 크롤링
                 # dt = ''
@@ -85,7 +85,7 @@ for i in range(len(category)):                          # 카테고리 변경
                 #             data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
                 #             dt += data
                 #     print(dt)
-                #     titles.append(dt)
+                #     effect.append(dt)
 
                 # 3. 상품 설명 및 요약 크롤링
                 dt = ''
@@ -94,17 +94,17 @@ for i in range(len(category)):                          # 카테고리 변경
                     if not data.text == '':
                         data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
                         dt += data
-                titles.append(dt)
+                effect.append(dt)
 
             except:
                 # 오류가 발생할 경우 error + 카테고리, 페이지, 제품 번호 출력
                 print('error c.{} p.{} p.{}'.format(category[i], j, k))
 
-    df_nutrient = pd.DataFrame({'effect': titles, 'name': product_names})
+    df_nutrient = pd.DataFrame({'effect': effect, 'name': product_names})
     df_nutrient['category'] = category_kr[i]
     df_nutrients = pd.concat([df_nutrients, df_nutrient], ignore_index=True)
 
-    # crawling 폴더에 Nutrients_(카테고리)_(년월일).cvs 파일로 저장
+    # crawling 폴더에 Nutrients_(카테고리)_(년월일).cvs 카테고리 마다 중간 저장
     # df_nutrient.to_csv(
     #     './crawling_data/nutrients_{}_{}.csv'.format(category[i], datetime.datetime.now().strftime('%Y%m%d')),
     #     index=False)
